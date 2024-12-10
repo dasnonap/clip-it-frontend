50f2996cce637e2aaba31b792030884dffcac7ed
dasnonap <ivanmihov971@gmail.com>
2024-12-10 22:23:12 +0200

    feat(homepage): add header and footer components

---
+++ src/components/_comp/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div>
+      <h1>Helloo from the footerrr.</h1>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/_comp/header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div>
+      <h1>hello from the headerrrr!!</h1>
+    </div>
+  );
+}
+
+export default Header;
